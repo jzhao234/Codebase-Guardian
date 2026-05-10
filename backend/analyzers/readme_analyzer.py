2cf0050e78504b6b211db5be22c6f6419e77bc1f
Junhao Zhao <junzhao234@gmail.com>
2026-05-10 04:28:09 -0400

    did the same with read me file. function scans read me, saves results, writes to analsis json file with the other info

---
+++ backend/analyzers/readme_analyzer.py
@@ -0,0 +1,12 @@
+import re
+
+def analyze_readme(path):
+    with open(path, "r", encoding="utf-8") as file:
+        content = file.read()
+
+        npm_commands = re.finall(r"npm run ([a-zA-Z0-9:_=]+)", content)
+
+        return {
+            "path": path, 
+            "npm_run_commands": npm_commands,
+        }
--- backend/main.py
+++ backend/main.py
@@ -2,20 +2,26 @@ import json
 
 import repo_scanner as repo_scanner
 import analyzers.package_analyzer as package_analyzer
+import analyzers.readme_analyzer as readme_analyzer
 
 def main():
     repo_map = repo_scanner.scan_repo('.')
 
     package_results = []
+    readme_results = []
 
     for file_info in repo_map:
         if file_info["name"] == "package.json":
             result = package_analyzer.analyze_package_json(file_info["path"])
             package_results.append(result)
+        if file_info["name"].lower() == "readme.md":
+            result = readme_analyzer.analyze_readme(file_info["path"])
+            readme_results.append(result)
     
     output = {
         "repo_map": repo_map,
-        "package_analysis": package_results
+        "package_analysis": package_results,
+        "readme_analysis": readme_results,
     }
 
     with open("analysis_output.json", "w") as file:
